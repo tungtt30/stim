2eb3d1e817d9312f5b84ae6c2669f802d959fcdf
tungtt30 <tungchipsctn2@gmail.com>
2022-10-22 20:28:47 +0700

    fl

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Community = () => {
     return (
-        <div>Community</div>
+        <>
+            <div>Community</div>
+            <div className='flex items-center justify-center m-2'>
+                <img src="https://s11.flagcounter.com/count2/Bk1/bg_FFFFFF/txt_000000/border_CCCCCC/columns_3/maxflags_20/viewers_0/labels_0/pageviews_0/flags_0/percent_0/" alt="Flag Counter" border="3" />
+            </div>
+        </>
     )
 }
 
